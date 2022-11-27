604b66bb6248002964856bcab65fddc63cf1f105
Mark Bonchi <markbonchi@gmail.com>
2022-11-27 12:50:14 -0800

    Base Model

--- models/base_model.py
+++ models/base_model.py
@@ -11,12 +11,26 @@ class BaseModel:
     """
     The BaseModel class to parent subclasses
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialization for BaseModel"""
-        uniq_id = uuid.uuid4()
-        self.id = str(uniq_id)
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key,value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+            if kwargs.get("created_at", None) and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            else:
+                self.created_at = datetime.now()
+            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+            else:
+                self.updated_at = datetime.now()
+            if kwargs.get("id", None) is None:
+                self.id = str(uuid.uuid4())
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """String representation of the BaseModel"""
