f21b2d1201263fbf94496be577b653eb4fa1956c
Mark Bonchi <markbonchi@gmail.com>
2022-11-27 19:38:45 -0800

    Console

--- console.py
+++ console.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
-"""
-"""
+"""Console"""
 import cmd
 
 
+class HBNBCommand(cmd.Cmd):
+    """contains the entry point of the command interpreter"""
 
+    prompt = "(hbnb)"
+
+    def do_quit(self, line):
+        """To exit the program"""
+        return True
+
+    def do_EOF(self, line):
+        """exit prgram with ctrl + D"""
+        return True
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
