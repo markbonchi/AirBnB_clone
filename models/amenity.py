e7f7b8503b252420173d9e7bf329661af8ce6049
Mark Bonchi <markbonchi@gmail.com>
2022-11-28 02:24:05 -0800

    Console

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+""" holds class Amenity"""
+import models
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Representation of Amenity"""
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" holds class State"""
+import models
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Representation of Stast"""
+    name = ""
